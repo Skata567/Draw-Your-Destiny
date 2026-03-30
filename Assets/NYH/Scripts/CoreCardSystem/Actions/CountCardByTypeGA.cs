6ce435ddd76e822b74971ce9a9e14bd130c376f5
_2Cure <yunjiwoo0518@gmail.com>
2026-03-30 17:47:45 +0900

    AAA
    걍

--- Assets/NYH/Scripts/CoreCardSystem/Actions/CountCardByTypeGA.cs
+++ Assets/NYH/Scripts/CoreCardSystem/Actions/CountCardByTypeGA.cs
@@ -3,5 +3,10 @@ using UnityEngine;
 
 public class CountCardByTypeGA : GameAction
 {
-    
+	CardType _CardType;
+
+	public CountCardByTypeGA(CardType cardType)
+	{
+		_CardType = cardType;
+	}
 }
---
+++ Assets/NYH/Scripts/CoreCardSystem/Effect/CountCardByTypeEffect.cs
@@ -0,0 +1,13 @@
+﻿using NYH.CoreCardSystem;
+using UnityEngine;
+
+public class CountCardByTypeEffect : Effect
+{
+	[Header("찾을 카드 타입")]
+	[SerializeField] CardType cardType;
+
+	public override GameAction GetGameAction(int effectIndex = 0, Card sourceCard = null)
+	{
+		return new CountCardByTypeGA(cardType);
+	}
+}
